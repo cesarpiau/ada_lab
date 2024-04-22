d623d85fbbc86e35108605958b83bcc7720691a6
Cesar <cesarpiau@gmail.com>
2024-04-22 08:07:22 -0300

    configuração de remote backend do terraform

--- infraestrutura/main.tf
+++ infraestrutura/main.tf
@@ -1,5 +1,10 @@
 terraform {
-
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "sttfstateudemy"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    key                  = "k8s-ada-lab"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+  }
 }
 provider "azurerm" {
   features {}

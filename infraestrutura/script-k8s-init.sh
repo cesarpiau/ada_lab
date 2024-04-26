eefcc95bcbbc2e28fbf6e7fd188817b2cc159dbd
Cesar <cesarpiau@gmail.com>
2024-04-26 00:08:32 -0300

    criação do ingress

--- infraestrutura/script-k8s-init.sh
+++ infraestrutura/script-k8s-init.sh
@@ -19,3 +19,4 @@ echo 'complete -o default -F __start_kubectl k' | sudo tee -a $HOME/.bashrc
 exec bash
 
 # kubectl rollout restart deployment -n kube-system coredns
+# kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.10.1/deploy/static/provider/baremetal/deploy.yaml
